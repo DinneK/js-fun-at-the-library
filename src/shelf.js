871b5dfe4c05d106736c02b03a3299d632b7d088
Dinne Kopelevich <dinne22@hotmail.com>
2022-05-14 20:59:51 -0600

    Add shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,47 @@
+function shelfBook(book, sciFiShelf) {
+  if (book.genre === 'sciFi' && sciFiShelf.length < 3) {
+    sciFiShelf.unshift(book);
+  }
+};
+
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      shelf.splice(i, 1)
+    }
+  }
+};
+
+
+function listTitles(shelf) {
+  var titles = '';
+  for (var i = 0; i < shelf.length; i++) {
+    titles = titles + shelf[i].title + ', ';
+  }
+  return titles.substring(0, titles.length - 2);
+};
+
+
+
+function searchShelf(bookShelf, title) {
+  var available = null;
+  for (var i = 0; i < bookShelf.length; i++) {
+    if (bookShelf[i].title.includes(title)) {
+      return available = true;
+  } else {
+      available = false;
+    }
+  }
+  return available;
+};
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
